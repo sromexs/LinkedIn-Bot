44d810eccb1882e7c9905d38d955a5dbc04d41da
Sadra Bahrami <48912836+sromexs@users.noreply.github.com>
2021-03-19 14:49:21 +0330

    Changed the way of liking

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import {
+  clickByJavascript,
+  createData,
+  moveToElement,
+  buildDrive,
+  waitToFindElementByXpath,
+  sleep,
+} from "./functions/fn.index";
+
+(async () => {
+  createData();
+  const drive = await buildDrive();
+  await drive.manage().window().maximize();
+  let totalLikes = 0;
+
+  while (true) {
+    try {
+      await drive.get(
+        "https://www.linkedin.com/search/results/content/" +
+          "?keywords=javascript&origin=FACETED_SEARCH&sortBy=%22date_posted%22"
+      );
+
+      const firstProfileAnchor = await waitToFindElementByXpath(
+        drive,
+        "(//ul/child::li/child::div[@class='entity-result']" +
+          "[descendant::a[1][not(contains(@href,'company'))]])[1]" +
+          "/descendant::a[1]"
+      );
+
+      const profileLink = await firstProfileAnchor?.getAttribute("href");
+
+      if (!profileLink) {
+        throw new Error("String is empty!");
+      }
+
+      let link: string;
+      await drive.get((link = profileLink));
+
+      while (link.includes(profileLink)) {
+        await sleep(1000);
+        link = await drive.getCurrentUrl();
+      }
+
+      console.log({ link });
+
+      await drive.get(link + "detail/recent-activity/");
+
+      const firstLikeBtn = await waitToFindElementByXpath(
+        drive,
+        "//button[@aria-pressed='false' and contains(@aria-label,'Like')]"
+      );
+
+      if (!firstLikeBtn) {
+        throw new Error("First like button not found!");
+      }
+
+      await moveToElement(drive, firstLikeBtn);
+      await sleep(2000);
+      await clickByJavascript(drive, firstLikeBtn);
+
+      totalLikes++;
+      console.log({ totalLikes });
+
+      for (let i = 15; i > 0; i--) {
+        console.log({ status: "Sleep", minute: i });
+        await sleep(60000);
+      }
+    } catch (err) {
+      console.log(err);
+      await sleep(5 * 60000);
+    }
+  }
+})();
